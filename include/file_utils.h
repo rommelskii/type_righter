7ababa76f0a0f0d81474b8b20d4f655b9f5ac755
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-06 01:15:43 +0800

    [file utils] added header for obfuscator

--- include/file_utils.h
+++ include/file_utils.h
@@ -10,4 +10,10 @@
 */
 void file_save(struct DTkL* buflist, const char* PATH);
 
+/**
+* @brief Performs a fixed-buffer XOR obfuscation on a plaintext file to produce a binary file
+* @param buflist Token list
+*/
+void file_obfuscate(const char* PATH);
+
 #endif //FILE_UTILS_H_
