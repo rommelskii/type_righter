5fb5f14e8f72d5ef9283ccf57d608811371a1c94
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2026-02-06 15:12:07 +0800

    [file utils] transferred XOR logic to file utils

--- include/file_utils.h
+++ include/file_utils.h
@@ -1,8 +1,10 @@
 #ifndef FILE_UTILS_H_
 #define FILE_UTILS_H_
 
-
 #include "dtk.h"
+
+#define FILE_OBF_LEN 8192
+#define KEY "WeDoNotTalkAboutTheKey"
 
 /**
 * @brief Writes a token list to a file
@@ -14,6 +16,8 @@ void file_save(struct DTkL* buflist, const char* PATH);
 * @brief Performs a fixed-buffer XOR obfuscation on a plaintext file to produce a binary file
 * @param buflist Token list
 */
-void file_obfuscate(const char* PATH);
+void file_obfuscate(const char* ORIGINAL_PATH, const char* RESULT_PATH);
+
+void xor_cize_offset(char* str, size_t len, const char* key, size_t keylen, size_t file_offset);
 
 #endif //FILE_UTILS_H_
